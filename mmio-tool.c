52aafcbf93bad65218b11608313623642c5c83fa
techflashYT <officialTechflashYT@gmail.com>
2025-12-18 13:04:13 -0700

    Massive overhaul
    * Add (semi-)proper versioning
    * Add multiple-length support
    * Support I/O to GX EFB
    * Overhaul argument handling

--- mmio-tool.c
+++ mmio-tool.c
@@ -13,17 +13,32 @@
 #include <fcntl.h>
 #include <sys/mman.h>
 
+#define ARG_IDX_MODE 1
+#define ARG_IDX_LEN 2
+#define ARG_IDX_ADDR 3
+#define ARG_IDX_VAL 4
+
+/* program, mode, length, address */
+#define MIN_ARGS 4
+/* program, mode, length, address, value */
+#define MAX_ARGS 5
+
+
 static void usage(void) {
 	puts(
-"./mmio-tool [mode] [address] <value>\n"
+"./mmio-tool [mode] [address] [length] <value>\n"
 "Options:\n"
 "	MODE:		Required.  Either 'r' or 'w', for read, or write.\n"
 "\n"
 "	ADDRESS:	Required.  Hexadecimal address to access.\n"
 "\n"
+"	LENGTH:		Required.  Valid values: 1, 2, 4\n"
+"\n"
 "	VALUE:		Required for write, forbidden for read.\n"
 "			The value to write to the provided address.\n"
 "\n"
+"\n"
+"This is Wii-Linux mmio-tool v1.1.\n"
 	);
 }
 
@@ -49,7 +64,8 @@ static int addr2range(char *addr, uint32_t *_range, uint32_t *_off) {
 	}
 
 	range = val & 0x0FF00000;
-	if (range != 0x0C000000 && /* Legacy Flipper registers */
+	if (range != 0x08000000 && /* GX EFB */
+	    range != 0x0C000000 && /* Legacy Flipper registers */
 	    range != 0x0D000000 && /* Hollywood registers */
 	    range != 0x0D800000) { /* Hollywood registers (mirrored) */
 		printf("ERROR: Refusing to touch unknown register range: 0x%08X!  Typo?\n", range);
@@ -66,34 +82,41 @@ static int addr2range(char *addr, uint32_t *_range, uint32_t *_off) {
 }
 
 int main(int argc, char *argv[]) {
-	int fd, len, ret, mode;
+	int fd, len, mapLen, ret, mode;
 	uint32_t *mem, *tmp, range, off, val;
 	char *tmpStr;
 
 	/* 1 region */
-	len = 0x00f00000;
+	mapLen = 0x00f00000;
 
 	/* do we have the right number of args? */
-	if (argc < 3 || argc > 4) {
+	if (argc < MIN_ARGS || argc > MAX_ARGS) {
 		usage();
 		return 1;
 	}
 
 	/* valid mode? */
-	if (strcmp(argv[1], "r") == 0)
+	if (strcmp(argv[ARG_IDX_MODE], "r") == 0)
 		mode = 0;
-	else if (strcmp(argv[1], "w") == 0)
+	else if (strcmp(argv[ARG_IDX_MODE], "w") == 0)
 		mode = 1;
 	else {
 		usage();
 		return 1;
 	}
 
-	/* right args? */
-	if ((mode == 1 && argc != 4) || (mode == 0 && argc != 3)) {
+	/* valid length? */
+	len = strtoul(argv[ARG_IDX_LEN], &tmpStr, 10);
+	if (tmpStr == argv[ARG_IDX_LEN] || errno == EINVAL || errno == ERANGE || (len != 1 && len != 2 && len != 4)) {
+		printf("Invalid length: \"%s\"\n", argv[ARG_IDX_LEN]);
 		usage();
-		ret = 1;
-		goto out;
+		return 1;
+	}
+
+	/* right args? */
+	if ((mode == 1 && argc != MAX_ARGS) || (mode == 0 && argc != MIN_ARGS)) {
+		usage();
+		return 1;
 	}
 
 	/* setup */
@@ -102,12 +125,12 @@ int main(int argc, char *argv[]) {
 		perror("ERROR: open failed");
 		return 1;
 	}
-	if (addr2range(argv[2], &range, &off)) {
+	if (addr2range(argv[ARG_IDX_ADDR], &range, &off)) {
 		close(fd);
 		return 1;
 	}
 
-	mem = mmap(NULL, len,
+	mem = mmap(NULL, mapLen,
 		PROT_READ | PROT_WRITE,
 		MAP_SHARED, fd, range
 	);
@@ -121,30 +144,50 @@ int main(int argc, char *argv[]) {
 	tmp = mem + (off / sizeof(uint32_t));
 
 	if (mode == 0) {
-		val = *tmp;
-		printf("0x%08X: %08X\n", range + off, val);
+		if (len == 1) {
+			val = *(uint8_t *)tmp;
+			printf("0x%08X: %02X\n", range + off, val);
+		}
+		else if (len == 2) {
+			val = *(uint16_t *)tmp;
+			printf("0x%08X: %04X\n", range + off, val);
+		}
+		else if (len == 4) {
+			val = *(uint32_t *)tmp;
+			printf("0x%08X: %08X\n", range + off, val);
+		}
 		ret = 0;
 		goto out;
 	}
 
 	if (mode == 1) {
 		errno = 0;
-		val = strtoul(argv[3], &tmpStr, 16);
-		if (tmpStr == argv[3] || errno == EINVAL || errno == ERANGE) {
-			printf("ERROR: invalid value \"%s\"\n", argv[3]);
+		val = strtoul(argv[ARG_IDX_VAL], &tmpStr, 16);
+		if (tmpStr == argv[ARG_IDX_VAL] || errno == EINVAL || errno == ERANGE) {
+			printf("ERROR: invalid value \"%s\"\n", argv[ARG_IDX_VAL]);
 			usage();
 			ret = 1;
 			goto out;
 		}
-		*tmp = val;
 
-		printf("Successfully wrote 0x%08X to 0x%08X\n", val, range + off);
+		if (len == 1) {
+			*(uint8_t *)tmp = val;
+			printf("Successfully wrote 0x%02X to 0x%08X\n", val, range + off);
+		}
+		else if (len == 2) {
+			*(uint16_t *)tmp = val;
+			printf("Successfully wrote 0x%04X to 0x%08X\n", val, range + off);
+		}
+		else if (len == 4) {
+			*(uint32_t *)tmp = val;
+			printf("Successfully wrote 0x%08X to 0x%08X\n", val, range + off);
+		}
 		ret = 0;
 		goto out;
 	}
 
 out:
-	munmap(mem, len);
+	munmap(mem, mapLen);
 	close(fd);
 	return ret;
 }
